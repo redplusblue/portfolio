3d7a1fba766cedc7d0cfcc9ec13fcba4bf4e4ac9
redplusblue <samirkabra1161@gmail.com>
2023-10-13 17:14:45 -0400

    Added skill section modifications, name animation changes, and todo list for further work

--- main.js
+++ main.js
@@ -14,53 +14,55 @@ function scrollToCard(cardId) {
   }
 }
 
-// Change the active button when scrolling
-window.addEventListener("scroll", () => {
-  const scrollPos = window.scrollY;
-  let activeBtn = document.querySelector(".active-btn");
-  // If the object is empty, then the offsets have not been calculated
-  if (Object.keys(cardOffsets).length === 0 && scrollPos === 0) {
-    const cards = document.querySelectorAll(".card"); // Assuming you have a class 'card' for your cards
-    cards.forEach((card) => {
-      cardOffsets[card.id] = card.offsetTop;
-    });
-  }
-  if (Object.keys(cardOffsets).length === 0) {
-    // Store the current scroll position
-    if (scrollPos !== 0) {
-      window.scrollTo({
-        top: 0,
-        behavior: "auto",
-      });
-    }
-  }
-  if (
-    scrollPos < cardOffsets["card_1"] - 200 ||
-    scrollPos > cardOffsets["card_3"] + 200
-  ) {
-    if (activeBtn) {
-      activeBtn.classList.remove("active-btn");
-    }
-  } else if (scrollPos < cardOffsets["card_2"]) {
-    if (activeBtn) {
-      activeBtn.classList.remove("active-btn");
-    }
-    document.querySelector("#about-btn").classList.add("active-btn");
-  } else if (
-    scrollPos >= cardOffsets["card_2"] &&
-    scrollPos < cardOffsets["card_3"]
-  ) {
-    if (activeBtn) {
-      activeBtn.classList.remove("active-btn");
-    }
-    document.querySelector("#skills-btn").classList.add("active-btn");
-  } else if (scrollPos >= cardOffsets["card_3"]) {
-    if (activeBtn) {
-      activeBtn.classList.remove("active-btn");
-    }
-    document.querySelector("#projects-btn").classList.add("active-btn");
-  }
-});
+// Change the active button when scrolling - Closed for debugging
+// window.addEventListener("scroll", () => {
+//   const scrollPos = window.scrollY;
+//   let activeBtn = document.querySelector(".active-btn");
+//   // If the object is empty, then the offsets have not been calculated
+//   if (Object.keys(cardOffsets).length === 0 && scrollPos === 0) {
+//     const cards = document.querySelectorAll(".card"); // Assuming you have a class 'card' for your cards
+//     cards.forEach((card) => {
+//       cardOffsets[card.id] = card.offsetTop;
+//     });
+//   }
+//   if (Object.keys(cardOffsets).length === 0) {
+//     // Store the current scroll position
+//     if (scrollPos !== 0) {
+//       window.scrollTo({
+//         top: 0,
+//         behavior: "auto",
+//       });
+//     }
+//   }
+//   if (
+//     scrollPos < cardOffsets["card_1"] - 200 ||
+//     scrollPos > cardOffsets["card_3"] + 200
+//   ) {
+//     if (activeBtn) {
+//       activeBtn.classList.remove("active-btn");
+//     }
+//   } else if (scrollPos < cardOffsets["card_2"]) {
+//     if (activeBtn) {
+//       activeBtn.classList.remove("active-btn");
+//     }
+//     document.querySelector("#about-btn").classList.add("active-btn");
+//   } else if (
+//     scrollPos >= cardOffsets["card_2"] &&
+//     scrollPos < cardOffsets["card_3"]
+//   ) {
+//     if (activeBtn) {
+//       activeBtn.classList.remove("active-btn");
+//     }
+//     document.querySelector("#skills-btn").classList.add("active-btn");
+//   } else if (scrollPos >= cardOffsets["card_3"]) {
+//     if (activeBtn) {
+//       activeBtn.classList.remove("active-btn");
+//     }
+//     document.querySelector("#projects-btn").classList.add("active-btn");
+//   }
+// });
+
+// Idea: If at top, scroll to whatever card is asked for by using scrollTO(cardId)
 
 // Navbar buttons
 document.querySelectorAll(".nav-btn").forEach((btn) => {
@@ -98,4 +100,46 @@ document.querySelectorAll(".ui-btn").forEach((btn) => {
     text = "Read More" === text ? "Read Less" : "Read More";
     btn.querySelector("span").innerText = text;
   });
+});
+
+// Skill Selector
+let select = document.querySelector(".skill-selector");
+// Check current selected option
+let selected = select.querySelector(".selected");
+// Whene the select is changed
+select.addEventListener("change", (e) => {
+  // Get the selected option number
+  // 0 -> All; 1-> Frontend; 2-> Backend; 3-> Android
+  let option = select.options[select.selectedIndex];
+  // Print the numberof the selected option
+  alert(option.value);
+});
+
+// Skill Icons -> Draggable
+// Replace 'scrollable-element' with the ID or class of your horizontally scrollable element
+var skillsContainer = document.querySelector(".skills-container");
+var isMouseDown = false;
+var startX;
+var scrollLeft;
+
+skillsContainer.addEventListener("mousedown", function (e) {
+  isMouseDown = true;
+  startX = e.pageX - skillsContainer.offsetLeft;
+  scrollLeft = skillsContainer.scrollLeft;
+});
+
+skillsContainer.addEventListener("mouseup", function () {
+  isMouseDown = false;
+});
+
+skillsContainer.addEventListener("mouseleave", function () {
+  isMouseDown = false;
+});
+
+skillsContainer.addEventListener("mousemove", function (e) {
+  if (!isMouseDown) return;
+  e.preventDefault();
+  var x = e.pageX - skillsContainer.offsetLeft;
+  var walk = x - startX;
+  skillsContainer.scrollLeft = scrollLeft - walk;
 });
---
+++ todo.md
@@ -0,0 +1,26 @@
+# Tasks:
+
+## General Page
+
+    - [ ] Fix Background (Starry animation)
+        - [ ] Custom full page scrolling background
+    - [ ] Fix Navbar Buttons
+    - [ ] Fix Navbar - Mobile
+    - [ ] Add animation to welcome text
+
+## About Page:
+
+    - [ ]
+
+## Skills Section:
+
+    - [ ] Set skill img elements as div background (or transparent divs) to prevent dragging (Ghost images)
+    - [ ] Set up JS based skill navigation (And image loading) based on Option picked.
+
+## Projects Section[TBD]:
+
+    - [ ] Set up side scrolling project preview and quick info screens for at least 5 projects.
+
+## Contact Section:
+
+    - [ ] Redesign to match space theme
